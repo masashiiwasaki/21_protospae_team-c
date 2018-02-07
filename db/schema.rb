2f2221d3886c4a6d30cff197353802299efd6265
yuka-matsuura <appleidmtk@outlook.jp>
2018-02-07 11:08:09 +0900

    Add tags table
    Tagテーブルを作成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405022520) do
+ActiveRecord::Schema.define(version: 20180207015622) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20160405022520) do
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title", limit: 255
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false

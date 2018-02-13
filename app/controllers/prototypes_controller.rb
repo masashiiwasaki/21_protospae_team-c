0ddfaafa414a313e619aa87880645c2e3d4af6ae
Jun Miyakawa <fcfym630@gmail.com>
2018-02-13 10:59:40 +0900

    タグ機能（登録）の実装
    登録までできるようになりました！

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,7 +8,10 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
     @prototype.captured_images.build
-    @prototype.tags.build
+    3.times {
+      # 関連オブジェクトをbuild
+      @prototype.tags.build
+    }
   end
 
   def create
